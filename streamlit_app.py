be81ca73514d040b8780b9573832eb8383387eba
Igor Yaremenko <Igor.Yaremenko4@gmail.com>
2023-03-11 17:13:36 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit as sl
 
 sl.title("My Parents New Healthy Diner")
+sl.header(Breackfast Menu)
+sl.text("Onega 3 & Blueberry Oatmeal")
+sl.text("Kale, Spinach & Rocket Smeethie")
+sl.text("Hard-Boiled Free-Range Egg")
